506dc33ca1f79c3d0790ff73334d3031eb60fbfd
Abu Ubada <abcoding17@gmail.com>
2023-09-16 15:11:01 +0530

    implement basic app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <div className="card">
+        <p>Advice #</p>
+      </div>
+    </div>
+  )
 }
 
 export default App
