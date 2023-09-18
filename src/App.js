3c93f3084aa08153bac61f973d3f27eb438054bd
Abu Ubada <abcoding17@gmail.com>
2023-09-18 09:05:39 +0530

    feat: fetch & display advice slip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [adviceSlip, setAdviceSlip] = useState({})
+
+  useEffect(() => {
+    async function fetchAdvice() {
+      const res = await fetch(`https://api.adviceslip.com/advice`)
+      const adviceSlip = await res.json()
+      console.log('fetchAdvice()', adviceSlip.slip)
+      setAdviceSlip(adviceSlip.slip)
+    }
+    fetchAdvice()
+    console.log('App intialised')
+  }, [])
+
   return (
     <div className="App">
       <div className="card">
-        <p>Advice #</p>
+        <p>Advice #{adviceSlip.id}</p>
+        <p>"{adviceSlip.advice}"</p>
       </div>
     </div>
   )
