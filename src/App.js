f30dfd7ec9f22a1962aa97edc4abd323c2e810a1
Abu-Ubada2020 <abcoding17@gmail.com>
2023-11-23 12:55:08 +0530

    add content & styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import PatternDividerMobile from './svgs/PatternDividerMobile'
 
 function App() {
-  const [adviceSlip, setAdviceSlip] = useState({})
+	const [adviceSlip, setAdviceSlip] = useState({})
 
-  useEffect(() => {
-    async function fetchAdvice() {
-      const res = await fetch(`https://api.adviceslip.com/advice`)
-      const adviceSlip = await res.json()
-      console.log('fetchAdvice()', adviceSlip.slip)
-      setAdviceSlip(adviceSlip.slip)
-    }
-    fetchAdvice()
-    console.log('App intialised')
-  }, [])
+	useEffect(() => {
+		async function fetchAdvice() {
+			const res = await fetch(`https://api.adviceslip.com/advice`)
+			const adviceSlip = await res.json()
+			console.log('fetchAdvice()', adviceSlip.slip)
+			setAdviceSlip(adviceSlip.slip)
+		}
+		fetchAdvice()
+		console.log('App intialised')
+	}, [])
 
-  return (
-    <div className="App">
-      <div className="card">
-        <p>Advice #{adviceSlip.id}</p>
-        <p>"{adviceSlip.advice}"</p>
-      </div>
-    </div>
-  )
+	return (
+		<div className="App">
+			<div className="card">
+				<p>ADVICE #{adviceSlip.id}</p>
+				<p className="advice">"{adviceSlip.advice}"</p>
+				<PatternDividerMobile />
+			</div>
+		</div>
+	)
 }
 
 export default App
